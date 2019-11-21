62598db4cf123bf65769c38bd1aa31b47662aa56
github username <github email address>
2019-11-21 14:55:34 +0000

    dog name rendered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import './App.css';
+import './App.css'
 
-function App() {
+const dogUrl = "http://localhost:3000/pups"
+
+
+class App extends React.Component {
+    state = {
+      dogs: [],
+    };
+    fetchDogs(){
+      fetch(dogUrl)
+        .then(res => res.json())
+        .then(res => this.setState({dogs: res}))
+    }
+    componentDidMount (){
+      this.fetchDogs()
+    }
+    
+
+render(){
+  // console.log(this.state.dogs)
+  // const dogs = this.state.dogs
+  // console.log("dogs: ", dogs)
+  const singleDogName = this.state.dogs.map(dog =><span>{dog.name}</span> )
+  // console.log("singleDogName: ", singleDogName)
+  // debugger
   return (
-    <div className="App">
+  <div className="App">
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
       <div id="dog-bar">
-
+        {singleDogName}
       </div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
@@ -18,6 +41,7 @@ function App() {
       </div>
     </div>
   );
+}
 }
 
 export default App;
