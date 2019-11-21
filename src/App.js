1434a24926b509b0b95a35b62f11455a56ec2289
github username <github email address>
2019-11-21 16:01:16 +0000

    onClickFn

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
 import React from 'react';
 import './App.css'
+import DogInfo from  './DogInfo'
 
 const dogUrl = "http://localhost:3000/pups"
 
 
 class App extends React.Component {
-    state = {
-      dogs: [],
-    };
-    fetchDogs(){
-      fetch(dogUrl)
-        .then(res => res.json())
-        .then(res => this.setState({dogs: res}))
-    }
-    componentDidMount (){
-      this.fetchDogs()
-    }
-    
+  state = {
+    dogs: [],
+    selectedDog: null,
+  };
 
-render(){
-  // console.log(this.state.dogs)
-  // const dogs = this.state.dogs
-  // console.log("dogs: ", dogs)
-  const singleDogName = this.state.dogs.map(dog =><span>{dog.name}</span> )
-  // console.log("singleDogName: ", singleDogName)
-  // debugger
-  return (
-  <div className="App">
-      <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
-      </div>
-      <div id="dog-bar">
-        {singleDogName}
-      </div>
-      <div id="dog-summary-container">
-        <h1>DOGGO:</h1>
-        <div id="dog-info">
+  fetchDogs(){
+    fetch(dogUrl)
+      .then(res => res.json())
+      .then(res => this.setState({dogs: res}))
+  }
 
+  componentDidMount (){
+    this.fetchDogs()
+  }
+  
+  handleDogClick = (dog) => {
+    this.setState({selectedDog: dog})
+  }
+
+  render(){
+    // console.log(this.state.dogs)
+    // const dogs = this.state.dogs
+    // console.log("dogs: ", dogs)
+    const dogNameSpans = this.state.dogs.map(dog =><span key={dog.id} onClick={(e) => this.handleDogClick(dog)}>{dog.name}</span> )
+    // console.log("singleDogName: ", singleDogName)
+    // debugger
+    return (
+    <div className="App">
+        <div id="filter-div">
+          <button id="good-dog-filter">Filter good dogs: OFF</button>
+        </div>
+        <div id="dog-bar">
+          {dogNameSpans}
+        </div>
+        <div id="dog-summary-container">
+          <h1>DOGGO:</h1>
+          <div id="dog-info">
+            {this.state.selectedDog ? <DogInfo slectedtDoggie={this.state.props}/> : null}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
---
+++ src/DogInfo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+
+const DogInfo =() => {
+
+    return <div> Doggie</div>
+}
+
+export default DogInfo;
